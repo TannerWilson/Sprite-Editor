3d5335c768a1d8f37e61a539f982f94882e69cb2
Jackson W Murphy <jmurphy@lab1-13.eng.utah.edu>
2016-03-29 13:42:56 -0600

    Add color swapping feature to primary/secondary color buttons

--- SpriteEditor/mainwindow.cpp
+++ SpriteEditor/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,9 +11,22 @@ MainWindow::MainWindow(QWidget *parent) :
     // Set the primary and secondary color choices
     ui->primaryColorButton->setStyleSheet("background-color: red");
     ui->secondaryColorButton->setStyleSheet("background-color: yellow");
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// Sets the current drawing color to the color displayed on the partially hidden color button.
+// Swaps the colors shown on the primary/secondary button pair.
+void MainWindow::on_secondaryColorButton_clicked()
+{
+    // Swap the colors shown on the two buttons
+    QString styleInfo = (ui->primaryColorButton->styleSheet());
+    ui->primaryColorButton->setStyleSheet(ui->secondaryColorButton->styleSheet());
+    ui->secondaryColorButton->setStyleSheet(styleInfo);
+
+
 }
