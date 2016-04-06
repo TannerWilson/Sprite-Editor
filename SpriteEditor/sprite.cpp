6ff892b0fba88d107db8f1a4307a5720f04dc6b5
TannerWilson <tanner28aw@gmail.com>
2016-04-05 20:23:22 -0600

    Added method header comments

--- SpriteEditor/sprite.cpp
+++ SpriteEditor/sprite.cpp
@@ -5,6 +5,9 @@ Sprite::Sprite()
     currentImageIndex = 0;
 }
 
+/*
+* Adds an image to the current sprite
+*/
 Image* Sprite::addImage()
 {
     Image* newimage = new Image();
@@ -12,6 +15,9 @@ Image* Sprite::addImage()
     return newimage;
 }
 
+/*
+* Deletes an image from the current sprite
+*/
 void Sprite::deleteImage(int index)
 {
     images.erase(images.begin() + index);
@@ -21,16 +27,25 @@ void Sprite::deleteImage(int index)
     }
 }
 
+/*
+* Returns the image at the given index
+*/
 Image* Sprite::getImage(int index)
 {
     return images.at(index);
 }
 
+/*
+* Returns the current selected image's index
+*/
 int Sprite::getCurrentImageIndex()
 {
     return currentImageIndex;
 }
 
+/*
+* Sets the index for which image is currently selected
+*/
 Image* Sprite::setCurrentImageIndex(int index)
 {
     currentImageIndex = index;
